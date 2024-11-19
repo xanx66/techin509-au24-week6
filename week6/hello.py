21d4e3185c1f645ee57a30e02db6314eb1e6a8ba
plmkna <106205763+plmkna@users.noreply.github.com>
2024-11-18 18:34:46 -0800

    add week 8

--- week6/hello.py
+++ week6/hello.py
@@ -1,2 +1,11 @@
+"""ls - list directory
+   cd - change directory
+   pwd - print working directory
+   For Mac
+   ------------
+   xcode-select --install
+"""
+
 print("Hello,world")
  
+"git bashs"
---
+++ week8/hello.py
@@ -0,0 +1,20 @@
+print ("Hello, Python" )
+# Step 1. Read the csv
+
+# Step 2. Loop through each row
+
+# Step 3. For each row,
+
+# Step 4. Process the data
+
+# Step 4a. Capitalize the values in each column when xxxxx
+
+# cap_value = value. upper()
+
+# Step 4b. Convert to string if the value is a number
+
+# Step 5. Append the new rows into the output list
+
+# output. append (cap_value)
+
+# Step 6. Write the output list into a new CSV file
